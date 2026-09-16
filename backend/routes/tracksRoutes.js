07269d063b082ec9663ae0035a5f281b33d71593
AbdoSalama999 <bdhhlslalamh@gamil.com>
2026-09-16 22:17:18 +0300

    final completed copy the frist version

--- backend/routes/tracksRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const { getAllTracks, createTrack } = require('../controllers/trackController')
-const router = express.Router()
-router.get('/', getAllTracks)
-router.post('/', createTrack)
-module.exports = router
